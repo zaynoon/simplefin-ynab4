e56b08326b008e719bcbdf8d1efd348fc448ccfc
Ahmed El Zein <ahmed@azein.com>
2023-03-13 23:30:36 -0500

    bug fixes

--- simplefin-ynab4.py
+++ simplefin-ynab4.py
@@ -7,6 +7,8 @@ import requests
 import base64
 import datetime
 import configparser
+import click
+from humanfriendly import parse_date
 
 
 from typing import Any, List, Tuple, Dict, Optional
@@ -69,58 +71,83 @@ def get_accounts(
 
 
 def ts_to_date(ts: int) -> str:
-    return datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d")
+    return datetime.datetime.fromtimestamp(ts).strftime("%m/%d/%Y")
 
 
 def to_csv(transactions: List[Dict[str, Any]]) -> List[str]:
     """
-    convert an array of transaction to an array of csv lines
+    convert an array of transaction to an array of csv lines for ynab4
 
     example data:
-    "Date","Payee","Memo","Amount"
-    "2016-01-01","Grocery Store","Import Memo","$13.37"
+    Date,Payee,Category,Memo,Outflow,Inflow
+    07/25/10,Sample Payee,,Sample Memo for an outflow,100.00,
+    07/26/10,Sample Payee 2,,Sample memo for an inflow,,500.00
+
     """
     # start with header
-    csv_lines = ['"Date","Payee","Memo","Amount"']
+    csv_lines = ["Date,Payee,Category,Memo,Outflow,Inflow\n"]
     for t in transactions:
         date = ts_to_date(t["posted"])
         payee = t["description"]
-        amount = t["amount"]
+        amount = float(t["amount"])
         memo = t.get("extra") or ""
-        csv_lines.append(f'"{date}","{payee}","{memo}","{amount}"')
+
+        inflow = ""
+        outflow = ""
+        if amount < 0:
+            outflow = abs(amount)
+        else:
+            inflow = abs(amount)
+
+        csv_lines.append(f'"{date}","{payee}",,"{memo}","{outflow}","{inflow}"' + "\n")
 
     return csv_lines
 
 
-def main() -> None:
+@click.command("import")
+@click.option("--start-time", help="override start time to import transactions form")
+def import_transactions(start_time: str) -> None:
     if "auth" not in config:
         setup_token = input("Setup Token? ")
         setup(setup_token)
 
-    last_access = int(config["simplefin"]["last_access_time"])
-
     now = int(time.time())
+
+    if start_time:
+        last_access = int(datetime.datetime(*parse_date(start_time)).timestamp())
+    else:
+        last_access = int(config["simplefin"]["last_access_time"]) or now - (
+            7 * 24 * 60 * 60
+        )
 
     data = get_accounts(start_date=last_access, end_date=now)
 
     if data.get("errors"):
         raise Exception(data.get("errors"))
 
+    count = 0
     for account in data["accounts"]:
         name = account["name"]
         name = re.sub(r"[^\w\s-]", "", name.lower())
         name = re.sub(r"[-\s]+", "-", name).strip("-_")
 
         csv_lines = to_csv(account["transactions"])
+        count += len(csv_lines) - 1  # do not count the header
+
+        if len(csv_lines) < 2:
+            # skip accounts with no transactions
+            continue
+
         csv_path = Path.home() / config["ynab"]["output_dir"] / f"{name}.csv"
         if not csv_path.parent.exists():
             csv_path.parent.mkdir()
         with open(csv_path, "wt") as file:
             file.writelines(csv_lines)
 
+    print(f"{count} transactions downloaded from {ts_to_date(last_access)} to now.")
     config["simplefin"]["last_access_time"] = str(now)
     update_config()
 
 
 if __name__ == "__main__":
-    main()
+    import_transactions()
